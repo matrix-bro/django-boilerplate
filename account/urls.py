2c9d9c546602a62bb6ea5d53200c266cda1a27ac
matrix-bro <razmhz11@gmail.com>
2024-02-14 20:13:56 +0545

    feat: added login and token refresh endpoint from jwt

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from account.api import account
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', account.RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
